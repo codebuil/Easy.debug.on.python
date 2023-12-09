6dc46612235f04dd806d8a59fbe36627ec3decd9
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-12-09 13:45:56 +0000

    Add files via upload

---
+++ formats.py
@@ -0,0 +1,28 @@
+class fileformat:
+    def __init__(self,files):
+        self.contents=""
+        f1=open(files,"r")
+        self.contents=f1.read()
+        f1.close()
+
+    def report(self,i):
+        ss=self.contents.split("\n")
+        for n in ss:
+            count=0
+            tt=True
+            while tt:
+                nn=count+i
+                if nn>len(n):
+                    nn=len(n)
+                    tt=False
+                n=n.replace("\r","")
+                print(n[count:nn])
+                count+=i
+                if count>len(n):
+                    tt=False 
+                 
+                
+      
+print("\x1bc\x1b[43;30m")
+f1=fileformat("my.txt")
+f1.report(20)
